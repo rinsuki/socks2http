682e17ba327df0c7f2ab4972b46d34b133b41695
rinsuki <428rinsuki+git@gmail.com>
2018-10-07 16:40:28 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/elazarl/goproxy"
+	"golang.org/x/net/proxy"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %s [http proxy listen port] [socks5 host] [socks5 port]", os.Args[0])
+		os.Exit(1)
+	}
+	socks5 := os.Args[2] + ":" + os.Args[3]
+	os.Setenv("HTTP_PROXY", "socks5://"+socks5)
+	fmt.Print(os.Args)
+	sock, err := proxy.SOCKS5("tcp", socks5, nil, proxy.Direct)
+	if err != nil {
+		fmt.Println("connect to socks5 proxy error", err)
+		os.Exit(2)
+	}
+
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.ConnectDial = func(network string, address string) (net.Conn, error) {
+		return sock.Dial(network, address)
+	}
+	http.ListenAndServe("localhost:"+os.Args[1], proxy)
+}
